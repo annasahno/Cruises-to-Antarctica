1787760d3153d7c9cb01c55486f3cefae7a00557
Anna <lilimp8@gmail.com>
2022-11-18 23:51:17 +0600

    добавляет исправления перед проверкой

--- source/js/modules/header.js
+++ source/js/modules/header.js
@@ -1,16 +1,37 @@
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.header__overlay');
 const mainNavigation = document.querySelector('.header__wrapper');
 const navigationToggleButton = document.querySelector('.header__toggle');
 const headerLinks = document.querySelectorAll('.header__link');
 
+const closeMenu = () => {
+  mainNavigation.classList.add('header__wrapper--closed');
+  mainNavigation.classList.remove('header__wrapper--opened');
+  header.classList.remove('is-active');
+  document.body.style.overflow = 'auto';
+};
+
+const openMenu = () => {
+  mainNavigation.classList.remove('header__wrapper--closed');
+  mainNavigation.classList.add('header__wrapper--opened');
+  header.classList.add('is-active');
+  document.body.style.overflow = 'hidden';
+};
+
 const toggleMenu = () => {
-  if (mainNavigation && navigationToggleButton) {
+  if (overlay) {
+    overlay.addEventListener('click', () => {
+      closeMenu();
+    });
+  }
+
+  if (mainNavigation && navigationToggleButton && header) {
     navigationToggleButton.addEventListener('click', function () {
       if (mainNavigation.classList.contains('header__wrapper--closed')) {
-        mainNavigation.classList.remove('header__wrapper--closed');
-        mainNavigation.classList.add('header__wrapper--opened');
+        openMenu();
+
       } else {
-        mainNavigation.classList.add('header__wrapper--closed');
-        mainNavigation.classList.remove('header__wrapper--opened');
+        closeMenu();
       }
     });
   }
@@ -19,8 +40,7 @@ const toggleMenu = () => {
 const jumpToSection = () => {
   headerLinks.forEach((item) => {
     item.addEventListener('click', () => {
-      mainNavigation.classList.add('header__wrapper--closed');
-      mainNavigation.classList.remove('header__wrapper--opened');
+      closeMenu();
     });
   });
 };
